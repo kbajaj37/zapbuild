5237276c2a6db1e90fe8a3b78ed27d4ca765e449
avikunjZapbuild <52025080+avikunjZapbuild@users.noreply.github.com>
2019-06-20 11:58:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
-function opennav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "260px";
+  document.getElementById("main").style.marginLeft = "260px";
 }
 
-function closenav() {
+function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.marginLeft= "0";
 }
+
