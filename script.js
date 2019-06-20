67a48faeeffab729b0a7de409811294ec0643b64
kbajaj37 <52025070+kbajaj37@users.noreply.github.com>
2019-06-19 23:00:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function opennav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closenav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
